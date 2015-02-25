6b8381ea5a1ca90bf7c3e18697c70afeabd9d253
Marshall Pierce <marshall.pierce@aexp.com>
2015-02-25 10:51:39 -0700

    Untested impl of SkybarRegistry

--- src/main/java/org/wtf/skybar/registry/SkybarRegistry.java
+++ src/main/java/org/wtf/skybar/registry/SkybarRegistry.java
@@ -1,42 +1,59 @@
 package org.wtf.skybar.registry;
 
-import java.lang.invoke.*;
+import java.lang.invoke.CallSite;
+import java.lang.invoke.MethodHandles;
+import java.lang.invoke.MethodType;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.LongAdder;
+import javax.annotation.Nonnull;
+import javax.annotation.concurrent.ThreadSafe;
 
-/**
- *
- */
+@ThreadSafe
 public class SkybarRegistry {
 
-    public static  SkybarRegistry registry = new SkybarRegistry();
+    public static final SkybarRegistry registry = new SkybarRegistry();
 
     // Holds the count of each line of each source file
-    private Map<String, Map<Integer, LongAdder>> visits = new ConcurrentHashMap<>();
+    private final Map<String, Map<Integer, LongAdder>> visits = new ConcurrentHashMap<>();
 
-    public Map toJson() {
+    @Nonnull
+    public Map<String, Map<Integer, Integer>> toJson() {
         //TODO: We need to return a JSON map representing the covered lines for each source file
-        return new HashMap();
+        return new HashMap<>();
     }
 
     /**
      * Called each time a line is visted
      */
     public void visitLine(String sourceFileName, int lineNumber) {
-        // TODO: Count the line visits
+        visits.get(sourceFileName).get(lineNumber).increment();
     }
-
 
     public void registerLine(String sourceName, int lineNumber) {
-        // TODO: Called at class load time. Initialize each existing line number in the class to count 0
+        visits.compute(sourceName, (sourceFile, map) -> {
+            if (map == null) {
+                // no map for the source file; create one with a single entry with a 0 count
+                HashMap<Integer, LongAdder> newCounterMap = new HashMap<>();
+                newCounterMap.put(lineNumber, new LongAdder());
+                return newCounterMap;
+            }
+
+            // map already exists for this source name; insert a new adder
+            if (map.put(lineNumber, new LongAdder()) != null) {
+                throw new IllegalStateException(
+                        "Duplicate call for source <" + sourceName + "> and line <" + lineNumber + ">");
+            }
+
+            return map;
+        });
     }
 
-    public static CallSite bootstrap(MethodHandles.Lookup caller, String name, MethodType type, String sourceName, int lineNumber) throws NoSuchMethodException, IllegalAccessException {
+    public static CallSite bootstrap(MethodHandles.Lookup caller, String name, MethodType type, String sourceName,
+            int lineNumber) throws NoSuchMethodException, IllegalAccessException {
         // TODO: (Advanced!)
         // Create a MethodHandle for the counter and return a ConstantCallSite for that
         return null;
     }
-
 }
