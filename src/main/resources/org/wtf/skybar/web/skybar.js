91d2d565b927c8f099b7de2660eabcbbab0007e7
Todd Costella <ToddCostella@gmail.com>
2015-02-25 16:31:07 -0700

    Added feature to allow for a different package to be instrumented as well as a differ port for the server. These are passed as system properties
    skybar.includedPackage
    skybar.serverPort
    
    If skybar.includedPackage is not specified, the server will not start.
    If skybar.serverPort is not specified the server will use 4321
    
    See RUNNING.txt for an example command with these updated properties.

--- src/main/resources/org/wtf/skybar/web/skybar.js
+++ src/main/resources/org/wtf/skybar/web/skybar.js
@@ -1,100 +1,85 @@
 angular.module('skybar', [])
-  .controller('SkybarController', ['$scope', '$interval', '$http', '$window', function ($scope, $interval, $http, $window) {
-    $scope.sourceFiles = function () {
-      var sourceFiles = [];
-      for (var sourceFile in $scope.coverage) {
-        sourceFiles.push(sourceFile);
-        //console.log(sourceFile);
-      }
-      return sourceFiles;
-    };
+    .controller('SkybarController', ['$scope', '$interval', '$http', '$window', function ($scope, $interval, $http, $window) {
+        $scope.sourceFiles = function () {
+            var sourceFiles = [];
+            for (var sourceFile in $scope.coverage) {
+                sourceFiles.push(sourceFile);
+                //console.log(sourceFile);
+            }
+            return sourceFiles;
+        };
 
 
-    $scope.loadSource = function(sourceFile) {
-      console.log("sourceFile = "+sourceFile)
-      $http.get(
-        '/source/' + sourceFile
-      ).success(function (data) {
+        $scope.loadSource = function (sourceFile) {
+            console.log("sourceFile = " + sourceFile)
+            $http.get(
+                    '/source/' + sourceFile
+            ).success(function (data) {
 
-          var sourceLines = data.split("\n")
-          $scope.sourceLines = []
+                    var sourceLines = data.split("\n")
+                    $scope.sourceLines = []
 
-          for(var lineNum in sourceLines) {
+                    for (var lineNum in sourceLines) {
 
-            var lineNumKey = (lineNum - 0 + 1).toString();
-            var execCount =  $scope.coverage[sourceFile][lineNumKey] || 0
+                        var lineNumKey = (lineNum - 0 + 1).toString();
+                        var execCount = $scope.coverage[sourceFile][lineNumKey] || 0
 
-            console.log("execCount for "+lineNumKey+ " = "+execCount)
-
-
-             $scope.sourceLines.push({ "text": sourceLines[lineNum], "execCount": execCount })
-          }
-          $scope.currentSourceFile = sourceFile
-
-          console.log($scope.sourceLines)
-        }).error(function (data, status) {
-          console.log("error loading coverage data:");
-          console.log("status: " + status);
-          console.log("data: " + data);
-          $window.alert("Error loading coverage data.")
-        })
-    }
-
-    $scope.getExecCount = function(coverage, sourceFile, lineNumber) {
-      var sourceFileCoverage = coverage[sourceFile]
-      return sourceFileCoverage[lineNumber.toString()]
-    }
-    function updateCoverage() {
-        $http.get(
-          '/coverage.json'
-        ).success(function (data) {
-            //console.log("data: " + JSON.stringify(data));
-            $scope.coverage = data
-          }).error(function (data, status) {
-            console.log("error loading coverage data:");
-            console.log("status: " + status);
-            console.log("data: " + data);
-            $window.alert("Error loading coverage data.")
-          })
-      }
-    updateCoverage();
-    $interval(updateCoverage, 2000);
+                        console.log("execCount for " + lineNumKey + " = " + execCount)
 
 
-  }]).controller('WebSocketController', ['$scope', '$window', function ($scope, $window) {
-    var wsUri = "ws://localhost:4321/time/";
+                        $scope.sourceLines.push({ "text": sourceLines[lineNum], "execCount": execCount })
+                    }
+                    $scope.currentSourceFile = sourceFile
 
-    var websocket = new WebSocket(wsUri);
-    websocket.onopen = function (evt) {
-      onOpen(evt)
-    };
-    websocket.onclose = function (evt) {
-      onClose(evt)
-    };
-    websocket.onmessage = function (evt) {
-      onMessage(evt)
-    };
-    websocket.onerror = function (evt) {
-      onError(evt)
-    };
+                    console.log($scope.sourceLines)
+                }).error(function (data, status) {
+                    console.log("error loading coverage data:");
+                    console.log("status: " + status);
+                    console.log("data: " + data);
+                    $window.alert("Error loading coverage data.")
+                })
+        }
 
-    function onOpen(evt) {
-      console.log("onOpen Event")
-    };
+        $scope.getExecCount = function (coverage, sourceFile, lineNumber) {
+            var sourceFileCoverage = coverage[sourceFile]
+            return sourceFileCoverage[lineNumber.toString()]
+        }
+        function updateCoverage() {
+            $http.get(
+                '/coverage.json'
+            ).success(function (data) {
+                    //console.log("data: " + JSON.stringify(data));
+                    $scope.coverage = data
+                }).error(function (data, status) {
+                    console.log("error loading coverage data:");
+                    console.log("status: " + status);
+                    console.log("data: " + data);
+                    $window.alert("Error loading coverage data.")
+                })
+        }
 
-    function onClose(evt) {
-      console.log("onClose Event")
-    };
+        updateCoverage();
+        $interval(updateCoverage, 2000);
 
-    function onMessage(evt) {
-      var parsed = JSON.parse(evt.data);
-      $scope.time = parsed.time;
-      $scope.$digest();
 
-    };
+    }]).controller('WebSocketController', ['$scope', '$window', function ($scope, $window) {
+        var host = location.host;
+        var wsUri = "ws://" + host + "/time/";
+        var websocket = new WebSocket(wsUri);
 
-    function onError(evt) {
-      $window.alert("onError event")
-    };
+        websocket.onopen = function (evt) {
+            console.log("onOpen Event")
+        };
+        websocket.onclose = function (evt) {
+            console.log("onClose Event")
+        };
+        websocket.onmessage = function (evt) {
+            var parsed = JSON.parse(evt.data);
+            $scope.time = parsed.time;
+            $scope.$digest();
+        };
+        websocket.onerror = function (evt) {
+            $window.alert("onError event")
+        };
 
-  }]);
+    }]);
