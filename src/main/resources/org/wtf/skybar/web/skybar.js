46320caca94ad8d4e47db0053ef6c19bb70ab4a6
Todd Costella <ToddCostella@gmail.com>
2015-02-25 14:00:34 -0700

    Added initial support for web sockets to the client. A new controller was added that opens the socket and registers the callback methods for the events.
    The onMessage function will put the time from the websocket into the scope.

--- src/main/resources/org/wtf/skybar/web/skybar.js
+++ src/main/resources/org/wtf/skybar/web/skybar.js
@@ -1,27 +1,64 @@
 angular.module('skybar', [])
-  .controller('SkybarController', ['$scope', '$interval', '$http', '$window', function ($scope, $interval, $http, $window) {
-    $scope.sourceFiles = function () {
-      var sourceFiles = [];
-      for (var sourceFile in $scope.coverage) {
-        sourceFiles.push(sourceFile);
-        console.log(sourceFile);
-      }
-      return sourceFiles;
-    };
+    .controller('SkybarController', ['$scope', '$interval', '$http', '$window', function ($scope, $interval, $http, $window) {
+        $scope.sourceFiles = function () {
+            var sourceFiles = [];
+            for (var sourceFile in $scope.coverage) {
+                sourceFiles.push(sourceFile);
+                console.log(sourceFile);
+            }
+            return sourceFiles;
+        };
 
-    function updateCoverage() {
-        $http.get(
-          '/coverage.json'
-        ).success(function (data) {
-            console.log("data: " + JSON.stringify(data));
-            $scope.coverage = data
-          }).error(function (data, status) {
-            console.log("error loading coverage data:");
-            console.log("status: " + status);
-            console.log("data: " + data);
-            $window.alert("Error loading coverage data.")
-          })
-      }
-    updateCoverage();
-    $interval(updateCoverage, 2000);
-  }]);
+        function updateCoverage() {
+            $http.get(
+                '/coverage.json'
+            ).success(function (data) {
+                    console.log("data: " + JSON.stringify(data));
+                    $scope.coverage = data
+                }).error(function (data, status) {
+                    console.log("error loading coverage data:");
+                    console.log("status: " + status);
+                    console.log("data: " + data);
+                    $window.alert("Error loading coverage data.")
+                })
+        }
+
+        updateCoverage();
+        $interval(updateCoverage, 2000);
+    }])
+    .controller('WebSocketController', ['$scope', '$window', function ($scope, $window) {
+        var wsUri = "ws://localhost:4321/time/";
+
+        var websocket = new WebSocket(wsUri);
+        websocket.onopen = function (evt) {
+            onOpen(evt)
+        };
+        websocket.onclose = function (evt) {
+            onClose(evt)
+        };
+        websocket.onmessage = function (evt) {
+            onMessage(evt)
+        };
+        websocket.onerror = function (evt) {
+            onError(evt)
+        };
+
+        function onOpen(evt) {
+            console.log("onOpen Event")
+        };
+
+        function onClose(evt) {
+            console.log("onClose Event")
+        };
+
+        function onMessage(evt) {
+            scope.time = JSON.parse(evt.data)
+            scope.$digest()
+
+        };
+
+        function onError(evt) {
+            $window.alert("onError event")
+        };
+
+    }]);
