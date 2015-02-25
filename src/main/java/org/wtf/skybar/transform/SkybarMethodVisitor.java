5bd3a225895102923ea21f8aeeebecbeee2c0ab7
Jack Leow <jleow@shopzilla.com>
2015-02-25 10:39:16 -0700

    Implemented method visitor.

--- src/main/java/org/wtf/skybar/transform/SkybarMethodVisitor.java
+++ src/main/java/org/wtf/skybar/transform/SkybarMethodVisitor.java
@@ -5,6 +5,7 @@ package org.wtf.skybar.transform;
  */
 
 import org.objectweb.asm.MethodVisitor;
+import org.wtf.skybar.registry.SkybarRegistry;
 import org.wtf.skybar.transform.util.WorkingLineNumberVisitor;
 
 class SkybarMethodVisitor extends WorkingLineNumberVisitor {
@@ -21,9 +22,16 @@ class SkybarMethodVisitor extends WorkingLineNumberVisitor {
         // TODO: This just outputs the count to System.out.
         // Instead, inject a call to SkybarRegistry.registry.visitLine()
         // Hint: HelloWorldAsm in tests will output the ASM code which produces HelloWorld's byte code
-        mv.visitFieldInsn(GETSTATIC, "java/lang/System", "out", "Ljava/io/PrintStream;");
-        mv.visitLdcInsn("Running line " + lineNumber + " of source file " + sourceFile);
-        mv.visitMethodInsn(INVOKEVIRTUAL, "java/io/PrintStream", "println", "(Ljava/lang/String;)V", false);
+//        mv.visitFieldInsn(GETSTATIC, "java/lang/System", "out", "Ljava/io/PrintStream;");
+//        mv.visitLdcInsn("Running line " + lineNumber + " of source file " + sourceFile);
+//        mv.visitMethodInsn(INVOKEVIRTUAL, "java/io/PrintStream", "println", "(Ljava/lang/String;)V", false);
+
+        mv.visitFieldInsn(GETSTATIC, "org/wtf/skybar/registry/SkybarRegistry", "registry", "Lorg/wtf/skybar/registry/SkybarRegistry;");
+        mv.visitLdcInsn(sourceFile);
+        mv.visitLdcInsn(lineNumber);
+        mv.visitMethodInsn(INVOKEVIRTUAL, "org/wtf/skybar/registry/SkybarRegistry", "visitLine", "(Ljava/lang/String;I)V", false);
+
+        SkybarRegistry.registry.registerLine(sourceFile, lineNumber);
     }
 
     @Override
