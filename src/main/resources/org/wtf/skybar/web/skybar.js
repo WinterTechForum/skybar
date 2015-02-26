635f094520a164a1cb41b8d5df011d4556bdbe00
Jack Leow <jleow@shopzilla.com>
2015-02-26 14:20:51 -0700

    Removed some unused variables.

--- src/main/resources/org/wtf/skybar/web/skybar.js
+++ src/main/resources/org/wtf/skybar/web/skybar.js
@@ -43,54 +43,50 @@ function openWebSocket($scope)
 }
 
 angular.module('skybar', [])
-    .controller('SkybarController', ['$scope', '$interval', '$http', '$window', function ($scope, $interval, $http, $window) {
-
+    .controller('SkybarController', ['$scope', '$interval', '$http', function ($scope, $interval, $http) {
 
     openWebSocket($scope)
 
     $scope.sourceFiles = function () {
-            var sourceFiles = [];
-            for (var sourceFile in $scope.coverage) {
-                sourceFiles.push(sourceFile);
-                //console.log(sourceFile);
-            }
-            return sourceFiles;
-        };
-
-
-        $scope.loadSource = function (sourceFile) {
-            console.log("sourceFile = " + sourceFile)
-            $http.get(
-                    '/source/' + sourceFile
-            ).success(function (data) {
-
-                    var sourceLines = data.split("\n")
-                    $scope.sourceLines = []
-
-                    for (var lineNum in sourceLines) {
-
-                        var lineNumKey = (lineNum - 0 + 1).toString();
-                        var execCount = $scope.coverage[sourceFile][lineNumKey] || 0
-
-                        console.log("execCount for " + lineNumKey + " = " + execCount)
-
-
-                        $scope.sourceLines.push({ "text": sourceLines[lineNum], "execCount": execCount })
-                    }
-                    $scope.currentSourceFile = sourceFile
-
-                    console.log($scope.sourceLines)
-                }).error(function (data, status) {
-                    console.log("error loading coverage data:");
-                    console.log("status: " + status);
-                    console.log("data: " + data);
-                    $window.alert("Error loading coverage data.")
-                })
+        var sourceFiles = [];
+        for (var sourceFile in $scope.coverage) {
+            sourceFiles.push(sourceFile);
         }
+        return sourceFiles;
+    };
 
-        $scope.getExecCount = function (coverage, sourceFile, lineNumber) {
-            var sourceFileCoverage = coverage[sourceFile]
-            return sourceFileCoverage[lineNumber.toString()]
-        }
+    $scope.loadSource = function (sourceFile) {
+        console.log("sourceFile = " + sourceFile)
+        $http.get(
+            '/source/' + sourceFile
+        ).success(function (data) {
 
-    }]);
+              var sourceLines = data.split("\n")
+              $scope.sourceLines = []
+
+              for (var lineNum in sourceLines) {
+
+                  var lineNumKey = (lineNum - 0 + 1).toString();
+                  var execCount = $scope.coverage[sourceFile][lineNumKey] || 0
+
+                  console.log("execCount for " + lineNumKey + " = " + execCount)
+
+
+                  $scope.sourceLines.push({ "text": sourceLines[lineNum], "execCount": execCount })
+              }
+              $scope.currentSourceFile = sourceFile
+
+              console.log($scope.sourceLines)
+          }).error(function (data, status) {
+              console.log("error loading coverage data:");
+              console.log("status: " + status);
+              console.log("data: " + data);
+          })
+    };
+
+    $scope.getExecCount = function (coverage, sourceFile, lineNumber) {
+        var sourceFileCoverage = coverage[sourceFile];
+        return sourceFileCoverage[lineNumber.toString()];
+    };
+
+}]);
