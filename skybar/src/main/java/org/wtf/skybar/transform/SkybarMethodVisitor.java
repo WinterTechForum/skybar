0d28a93de385c16c057a543ff65e3043623847a8
Eirik Bjørsnøs <eirbjo@gmail.com>
2015-03-08 13:54:11 +0100

    Only use InvokeDynamic on class files with version >= 1.7. Fall back to looking up the adder on older byte code.

--- skybar/src/main/java/org/wtf/skybar/transform/SkybarMethodVisitor.java
+++ skybar/src/main/java/org/wtf/skybar/transform/SkybarMethodVisitor.java
@@ -17,21 +17,37 @@ import java.lang.invoke.MethodType;
 class SkybarMethodVisitor extends WorkingLineNumberVisitor {
 
     private final String sourceFile;
+    private final int version;
 
-    public SkybarMethodVisitor(String sourceFile, MethodVisitor mv) {
+    public SkybarMethodVisitor(String sourceFile, int version, MethodVisitor mv) {
         super(ASM5, mv);
         this.sourceFile = sourceFile;
+        this.version = version;
     }
 
     @Override
     protected void onLineNumber(int lineNumber) {
         SkybarRegistry.registry.registerLine(sourceFile, lineNumber);
 
-        MethodType mt = MethodType.methodType(CallSite.class, MethodHandles.Lookup.class, String.class, MethodType.class, String.class, int.class);
+        if(useInvokeDynamic()) {
+            MethodType mt = MethodType.methodType(CallSite.class, MethodHandles.Lookup.class, String.class, MethodType.class, String.class, int.class);
 
-        Handle bootstrap = new Handle(Opcodes.H_INVOKESTATIC, Type.getInternalName(SkybarRegistry.class), "bootstrap",
-                mt.toMethodDescriptorString());
-        mv.visitInvokeDynamicInsn("visitLine", "()V", bootstrap, sourceFile, lineNumber);
+            Handle bootstrap = new Handle(Opcodes.H_INVOKESTATIC, Type.getInternalName(SkybarRegistry.class), "bootstrap",
+                    mt.toMethodDescriptorString());
+            mv.visitInvokeDynamicInsn("visitLine", "()V", bootstrap, sourceFile, lineNumber);
+        } else {
+            mv.visitFieldInsn(GETSTATIC, "org/wtf/skybar/registry/SkybarRegistry", "registry", "Lorg/wtf/skybar/registry/SkybarRegistry;");
+            mv.visitLdcInsn(sourceFile);
+            mv.visitLdcInsn(lineNumber);
+            mv.visitMethodInsn(INVOKEVIRTUAL, "org/wtf/skybar/registry/SkybarRegistry", "getAdderForLine", "(Ljava/lang/String;I)Ljava/util/concurrent/atomic/LongAdder;", false);
+            mv.visitInsn(LCONST_1);
+            mv.visitMethodInsn(INVOKEVIRTUAL, "java/util/concurrent/atomic/LongAdder", "add", "(J)V", false);
+
+        }
+    }
+
+    private boolean useInvokeDynamic() {
+        return version != Opcodes.V1_1 && version >= Opcodes.V1_7;
     }
 
     @Override
