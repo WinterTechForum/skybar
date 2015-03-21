daab6144d61b46af98164ad8e18b1267818867b5
Marshall Pierce <marshall@mpierce.org>
2015-03-21 16:27:13 -0700

    remove unused method

--- skybar/src/test/java/org/wtf/skybar/transform/SkybarClassVisitorTest.java
+++ skybar/src/test/java/org/wtf/skybar/transform/SkybarClassVisitorTest.java
@@ -193,17 +193,6 @@ public class SkybarClassVisitorTest {
         }, lineNums.length);
     }
 
-    @SuppressWarnings("unchecked")
-    private static ConcurrentMap<String, Map<Integer, LongAdder>> getVisits() {
-        try {
-            Field visits = SkybarRegistry.class.getDeclaredField("visits");
-            visits.setAccessible(true);
-            return (ConcurrentMap<String, Map<Integer, LongAdder>>) visits.get(SkybarRegistry.registry);
-        } catch (NoSuchFieldException | IllegalAccessException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
     private static IntLongMap linesOf(Class<?> clazz) {
         SkybarRegistry.DeltaListener deltaListener = (x) -> {
         };
