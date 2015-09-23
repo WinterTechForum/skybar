a31e7ba68c242adc56d9f8d136f763097851c179
Eirik Bjørsnøs <eirbjo@gmail.com>
2015-09-23 11:48:38 +0200

    Fix test to reflect updates in property names

--- skybar/src/test/java/org/wtf/skybar/agent/SkybarConfigTest.java
+++ skybar/src/test/java/org/wtf/skybar/agent/SkybarConfigTest.java
@@ -11,7 +11,7 @@ public class SkybarConfigTest {
     @Test
     public void testCheckSystemProperties() {
         HashMap<String, String> systemProps = new HashMap<>();
-        systemProps.put("skybar.webUi.port", "3");
+        systemProps.put("skybar.port", "3");
         SkybarConfig config = new SkybarConfig(new HashMap<>(), systemProps, new HashMap<>());
 
         assertEquals(3, config.getWebUiPort());
@@ -20,7 +20,7 @@ public class SkybarConfigTest {
     @Test
     public void testCheckEnvVar() {
         HashMap<String, String> env = new HashMap<>();
-        env.put("SKYBAR_WEBUI_PORT", "3");
+        env.put("SKYBAR_PORT", "3");
         SkybarConfig config = new SkybarConfig(new HashMap<>(), new HashMap<>(), env);
 
         assertEquals(3, config.getWebUiPort());
@@ -29,9 +29,9 @@ public class SkybarConfigTest {
     @Test
     public void testCheckSystemPropertiesBeforeFile() {
         HashMap<String, String> systemProps = new HashMap<>();
-        systemProps.put("skybar.webUi.port", "3");
+        systemProps.put("skybar.port", "3");
         HashMap<String, String> fileProps = new HashMap<>();
-        fileProps.put("skybar.webUi.port", "30000");
+        fileProps.put("port", "30000");
         SkybarConfig config = new SkybarConfig(fileProps, systemProps, new HashMap<>());
 
         assertEquals(3, config.getWebUiPort());
